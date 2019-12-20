8c94e4c03b91e7597f25df45c668ed845f68bb31
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2019-12-20 23:36:45 +0200

    add SimpleCenter

---
+++ SimpleCenter/SimpleCenter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCenter
+//
+//  Created by Volodymyr Ostapyshyn on 12/20/19.
+//  Copyright © 2019 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
