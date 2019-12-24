81c4c67032bb73ca3e99cd0dce059bc01ed2dfbf
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2019-12-24 23:46:16 +0200

    fix bugs

--- ElementQuiz/ElementQuiz/ViewController.swift
+++ ElementQuiz/ElementQuiz/ViewController.swift
@@ -37,6 +37,7 @@ class ViewController: UIViewController {
     
     
     @IBAction func gotoNextElement(_ sender: Any) {
+        
         currentElementIndex += 1
         if currentElementIndex == elementList.count {
             currentElementIndex = 0
