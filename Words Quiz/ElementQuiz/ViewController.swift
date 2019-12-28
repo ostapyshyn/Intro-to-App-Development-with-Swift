bdc8af1395eed41f96427041ab3a9b44c699a10d
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2019-12-28 23:48:46 +0200

    add Words Quiz

---
+++ Words Quiz/ElementQuiz/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by Volodymyr Ostapyshyn on 12/20/19.
+//  Copyright © 2019 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    
+    var currentElementIndex = 0
+    
+    func updateElement() {
+        answerLabel.text = "?"
+        let elementName = elementList[currentElementIndex]
+        
+        
+        let image = UIImage(named: elementName)
+        
+        imageView.image = image
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateElement()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    
+    
+    @IBAction func gotoNextElement(_ sender: Any) {
+        
+        currentElementIndex += 1
+        if currentElementIndex == elementList.count {
+            currentElementIndex = 0
+        }
+        updateElement()
+        
+    }
+    @IBAction func showAnswer(_ sender: Any) {
+        answerLabel.text = elementList[currentElementIndex]
+        
+    }
+    
+    
+    
+}
+
